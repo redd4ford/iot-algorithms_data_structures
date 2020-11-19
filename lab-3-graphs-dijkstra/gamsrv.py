62138e5a990376e020d1efbc7927e4c70fbf66d4
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-11-19 17:08:44 +0200

    Split main(), updated docstrings and doctests

--- lab-3-graphs-dijkstra/gamsrv.py
+++ lab-3-graphs-dijkstra/gamsrv.py
@@ -3,36 +3,32 @@ import doctest
 from graph import Graph, INT_MAX
 
 
-def main() -> None:
+def get_min_latency(graph: Graph, clients: list) -> int:
     """
-    get the total number of nodes, number of client nodes, and edges from
-    .in file. then, for each router, get latencies from it to all the clients.
-    find the max latency between the router and all the clients. if the max
-    latency is minimal, this router can be considered a good position
-    for the server.
-    :return: none
-    >>> with open('gamsrv.out', 'r') as file: print(file.readline())
+    using Dijkstra's algorithm, get latencies from each Router to all nodes.
+    find the max latency between the Router and all the Clients. if the max
+    latency is minimal, this Router can be considered a good position
+    for the Server.
+    :param graph: a Graph object
+    :param clients: list of numbers of nodes that are Clients (and cannot be
+    treated as Routers)
+    :return min_path_length: the minimum value of the greatest latency
+    to the Client
+    >>> graph = Graph()
+    >>> graph.add_two_sided_edge(1, 2, 20)
+    >>> graph.add_two_sided_edge(1, 4, 25)
+    >>> graph.add_two_sided_edge(2, 3, 20)
+    >>> graph.add_two_sided_edge(3, 4, 10)
+    >>> graph.add_two_sided_edge(4, 6, 60)
+    >>> graph.add_two_sided_edge(3, 5, 30)
+    >>> graph.add_two_sided_edge(5, 6, 10)
+    >>> clients = [1, 3, 6]
+    >>> print(get_min_latency(graph, clients))
     50
     """
-    graph = Graph()
-    with open('gamsrv.in', 'r') as in_file:
-        file_data = in_file.readlines()
-
-    nodes_count = int(file_data[0].split(' ')[0])
-    nodes = [node + 1 for node in range(0, nodes_count)]
-    clients = list(map(int, file_data[1].split(' ')))
-    # at this point edges contains lines of raw data: 'start_node end_node latency\n'
-    edges = file_data[2:]
-
-    for edge in edges:
-        # 'unpacking' edges
-        start_node, end_node, latency = list(map(int, edge.split(' ')))
-        graph.add_edge(start_node, end_node, latency)
-        graph.add_edge(end_node, start_node, latency)
-
     min_path_length = INT_MAX
 
-    for current_node in range(1, len(nodes) + 1):
+    for current_node in range(1, len(graph.nodes) + 1):
         if current_node not in clients:
             max_path_length = 0
             paths_to_all_nodes = graph.dijkstra(current_node)
@@ -43,10 +39,43 @@ def main() -> None:
 
             if min_path_length > max_path_length:
                 min_path_length = max_path_length
+    return min_path_length
 
-    open('gamsrv.out', 'w').write(str(min_path_length))
+
+def main(IN_FILE='gamsrv.in', OUT_FILE='gamsrv.out') -> None:
+    """
+    get the total number of nodes, number of client nodes, and edges from
+    .in file. then, find the Server location that minimizes the largest latency
+    value to the Client.
+    :return: none
+    >>> main('examples/1.in', 'examples/1.out')
+    >>> with open('examples/1.out', 'r') as file: print(file.readline())
+    100
+    >>> main('examples/2.in', 'examples/2.out')
+    >>> with open('examples/2.out', 'r') as file: print(file.readline())
+    10
+    >>> main('examples/3.in', 'examples/3.out')
+    >>> with open('examples/3.out', 'r') as file: print(file.readline())
+    1000000000
+    """
+    graph = Graph()
+    with open(IN_FILE, 'r') as in_file:
+        file_data = in_file.readlines()
+
+    clients = list(map(int, file_data[1].split(' ')))
+    # at this point edges contains lines of raw data: 'start_node end_node latency\n'
+    edges = file_data[2:]
+
+    for edge in edges:
+        # 'unpacking' edges
+        start_node, end_node, latency = list(map(int, edge.split(' ')))
+        graph.add_two_sided_edge(start_node, end_node, latency)
+
+    min_path_length = get_min_latency(graph, clients)
+
+    open(OUT_FILE, 'w').write(str(min_path_length))
 
 
 if __name__ == '__main__':
-    # doctest.testmod(verbose=True)
+    doctest.testmod(verbose=True)
     main()
